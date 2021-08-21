5a7abbfe4968817a33691a823bd1ac619bf0da47
albysola <albertosolavagione30@gmail.com>
2021-08-21 18:01:16 +0200

    eureka configured

--- OrderService/src/main/kotlin/it/polito/wa2/orderservice/OrderServiceApplication.kt
+++ OrderService/src/main/kotlin/it/polito/wa2/orderservice/OrderServiceApplication.kt
@@ -2,8 +2,10 @@ package it.polito.wa2.orderservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class OrderServiceApplication
 
 fun main(args: Array<String>) {
--- WarehouseService/src/main/kotlin/it/polito/wa2/warehouseservice/WarehouseServiceApplication.kt
+++ WarehouseService/src/main/kotlin/it/polito/wa2/warehouseservice/WarehouseServiceApplication.kt
@@ -2,8 +2,10 @@ package it.polito.wa2.warehouseservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class WarehouseServiceApplication
 
 fun main(args: Array<String>) {
